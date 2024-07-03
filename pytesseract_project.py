8eb5e432601c7bc78ca6d4246e6fe13813d76335
Nikunj Sondawale <115616498+3Nikunj@users.noreply.github.com>
2024-07-03 22:53:46 +0530

    Update pytesseract_project.py

--- pytesseract_project.py
+++ pytesseract_project.py
@@ -1,18 +1,17 @@
 import streamlit as st
 from PIL import Image
-import pytesseract
+import pytesseract as pyt
 import io
 
-# Create a file uploader widget
 uploaded_file = st.file_uploader(
     "Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Open the image using PIL
+
     image = Image.open(uploaded_file)
 
-    # Convert the image to text
-    text = pytesseract.image_to_string(image)
+    pyt.pytesserract.tesseract_cmd = "tesseract.exe"
+    text = pyt.image_to_string(image)
 
     # Display the original image
     st.image(image, caption="Uploaded Image")
